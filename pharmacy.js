7b97f4ab86935fdca73d9768a3f505ecd8cd64fc
Akmal Khan <82448668+akmal001007@users.noreply.github.com>
2023-10-16 11:56:55 +0430

    Add files via upload

---
+++ pharmacy.js
@@ -0,0 +1,9 @@
+// opening login tab 
+function pageRedirect(){
+    window.location.href = "login.html";
+}
+
+// opening purchase tab 
+function purchase(){
+    window.location.href = "purchase.html";
+}
